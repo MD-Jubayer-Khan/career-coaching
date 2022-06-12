2054bcdecd924d8be7834f6cd5ac644a05e0d724
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-12 23:03:00 +0600

    create login register toggle and requir auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../src/pages/shared/Header/Header';
-import './App.css';
+// import './App.css';
 import Home from '../src/pages/Home/Home/Home';
 import Checkout from '../src/pages/Checkout/Checkout';
 import Blogs from '../src/pages/Blogs/Blogs';
 import About from './pages/About/About';
 import NotFound from './pages/shared/NotFound/NotFound';
 import Footer from '../src/pages/shared/Footer/Footer'
+import Login from './pages/UserAuth/Login/Login';
+import Register from './pages/UserAuth/Register/Register';
+import RequireAuth from './pages/UserAuth/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -15,9 +18,16 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/checkout' element={<Checkout></Checkout>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/checkout" element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
+
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
-    // const [user] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-    // const handleSignOut = () =>{
-    //     signOut(auth);
+    const handleSignOut = () =>{
+        signOut(auth)};
     return (
          <>
             <Navbar collapseOnSelect expand="lg" sticky="top" variant="light" style={{height:'100px', backgroundColor:'#e0faff'}}>
@@ -19,20 +22,18 @@ const Header = () => {
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to="home">Home</Nav.Link>
                             <Nav.Link as={Link} to="checkout">Checkout</Nav.Link>
-                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
-                            <Nav.Link as={Link} to="about">About</Nav.Link>
-                            
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>         
                         </Nav>
-                        {/* <Nav>
+                        <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
                             {
                                 user ?
-                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+                                    <button className='btn btn-link text-primary text-decoration-none' onClick={handleSignOut}>sign out</button>
                                 :
                                 <Nav.Link as={Link} to="login">
                                 Login
                             </Nav.Link>}
-                        </Nav> */}
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
